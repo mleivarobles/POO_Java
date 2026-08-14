98538997221293cbcbc0dacd6fe9a7f393e8ba7e
Mario Leiva <mleiva.robles@gmail.com>
2026-08-13 21:34:14 -0400

    Se agrega linea de bienvenida

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,5 +5,6 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo!!!!!");
+        System.out.println("Bienvenido a POO - 2da Semana");
     }
 }
