425f73fa9b496bbb3c9437346d10c69288bd6bd1
Mario Leiva <mleiva.robles@gmail.com>
2026-08-13 21:50:05 -0400

    Clase Mascota

---
+++ Mascota.java
@@ -0,0 +1,18 @@
+/**
+ * Mascota
+ */
+public class Mascota {
+    //Atributos
+    String nombre;
+    String raza;
+    int edad;
+
+    //Metodos
+    void comer(){
+        System.out.println(nombre + " está comiendo");
+    }
+
+    void jugar(){
+        System.out.println(nombre + " está jugando");
+    }
+}
