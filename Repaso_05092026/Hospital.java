8ff189fa63a99f5a795852d54c7c411ef693c2d2
Mario Leiva <mleiva.robles@gmail.com>
2026-09-05 19:38:13 -0400

    Clase de repaso

---
+++ Repaso_05092026/Hospital.java
@@ -0,0 +1,17 @@
+public class Hospital {
+
+    String nombre;
+    String ciudad;
+    int cantidadCamas;
+
+    public Hospital(String nombre, String ciudad, int cantidadPiezas) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.cantidadCamas = cantidadPiezas;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospital [nombre=" + nombre + ", ciudad=" + ciudad + ", cantidadPiezas=" + cantidadPiezas + "]";
+    }
+}
