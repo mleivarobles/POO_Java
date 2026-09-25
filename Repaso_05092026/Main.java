24073ec8c92452fdd9b121593da5ae1084e1ddd9
Mario Leiva <mleiva.robles@gmail.com>
2026-09-24 22:04:00 -0300

    clase de repaso

--- Repaso_05092026/Main.java
+++ Repaso_05092026/Main.java
@@ -6,9 +6,11 @@ public class Main {
         int edad;
 
         Bus b1 = new Bus("Volvo", "V1678", "amarillo", 3, 4500, 28);
+        Bus b2 = new Bus("Scania", "T232", "azul-marino", 4, 10000, 45);
         
         System.out.println(h1);
         System.out.println(b1);
+        System.out.println(b2);
     }
     
 }
